ba047155dadf7fd1965553ae9aa469877b0ff201
EdwardEddyEd <imac@dhcp-210-39.lab.cs.dartmouth.edu>
2017-10-16 23:12:11 -0400

    Added base tabbar

--- miam/App.js
+++ miam/App.js
@@ -1,8 +1,77 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { 
+	StyleSheet, 
+	Text, 
+	View,
+	TabBarIOS } 
+from "react-native";
 import Feed from "./Components/Feed";
 export default class App extends React.Component {
-  render() {
-    return <Feed />;
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedTab: 'featured'
+		};
+	}
+	
+	render() {
+	    return (
+	    	<TabBarIOS selectedTab={this.state.selectedTab}>
+	    	<TabBarIOS.Item
+	    	selected={this.state.selectedTab === 'featured'}
+	    	systemIcon="featured"
+	    	onPress={() => {
+	    		this.setState({
+	    			selectedTab: 'featured',
+	    		});
+	    	}}>
+	    	<Feed/>
+	    	</TabBarIOS.Item>
+
+	    	<TabBarIOS.Item
+	    	selected={this.state.selectedTab === 'search'}
+	    	systemIcon="search"
+	    	onPress={() => {
+	    		this.setState({
+	    			selectedTab: 'search',
+	    		});
+	    	}}>
+	    	<Feed/>
+	    	</TabBarIOS.Item>
+
+	    	<TabBarIOS.Item
+	    	selected={this.state.selectedTab === 'more'}
+	    	systemIcon="more"
+	    	onPress={() => {
+	    		this.setState({
+	    			selectedTab: 'more',
+	    		});
+	    	}}>
+	    	<Feed/>
+	    	</TabBarIOS.Item>
+
+	    	<TabBarIOS.Item
+	    	selected={this.state.selectedTab === 'favorites'}
+	    	systemIcon="favorites"
+	    	onPress={() => {
+	    		this.setState({
+	    			selectedTab: 'favorites',
+	    		});
+	    	}}>
+	    	<Feed/>
+	    	</TabBarIOS.Item>
+
+	    	<TabBarIOS.Item
+	    	selected={this.state.selectedTab === 'contacts'}
+	    	systemIcon="contacts"
+	    	onPress={() => {
+	    		this.setState({
+	    			selectedTab: 'contacts',
+	    		});
+	    	}}>
+	    	<Feed/>
+	    	</TabBarIOS.Item>
+	    	</TabBarIOS>
+	    );
+	}
 }
