40b66dcb5721ae25dce8aa911d22ca7cf6f929ee
sophiajiang <sophiajxw@gmail.com>
2017-11-04 21:32:29 -0400

    profile bottom part

--- miam/Components/Profile.js
+++ miam/Components/Profile.js
@@ -19,21 +19,28 @@ import StatusBarColor from './StatusBarColor';
 import Heading from './Heading';
 
 var customData = require("../data/customData.json");
+var listData = require("../data/listData.json");
+
 const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 != r2 });
+const lv = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
 const vw = Dimensions.get("window").width;
 
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
+      dataSource: lv.cloneWithRows(['row 1', 'row 2']),
       postDataSource: ds.cloneWithRows([]),
-      loaded: false
+      loaded: false,
     };
   }
   componentDidMount() {
     this.setState({
+      dataSource: lv.cloneWithRows(listData),
       postDataSource: ds.cloneWithRows(customData),
-      loaded: true
+      loaded: true,
     });
   }
 
@@ -49,6 +56,33 @@ export default class Profile extends React.Component {
   }
 
   setEditorRef = (editor) => this.editor = editor
+
+  renderListView(post) {
+    var message = "";
+
+    if (post.event == "follow"){
+      message = "is now following you.";
+    }
+
+    if (post.event == "challenge"){
+      message = "challenged you!";
+    }
+
+    return (
+      <View style={styles.singleListContainer}>
+        <Image
+          style={styles.audienceProfile}
+          source={{uri: post.userProfile}}
+        />
+        <Text style={styles.message}>
+          {post.userName} {message} {'\n'}
+        </Text>
+        <Text style={styles.time}>
+          {post.time}
+        </Text>
+      </View>
+    );
+  }
 
   render() {
     return (
@@ -98,6 +132,16 @@ export default class Profile extends React.Component {
               <Text style={styles.fda}>Followering</Text>
             </View>
           </View>
+          <ListView
+            style={styles.listviewcontainer}
+            initialListSize={5}
+            enableEmptySections={true}
+            dataSource={this.state.dataSource}
+            renderRow={post => {
+              return this.renderListView(post);
+            }}
+            renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
+          />
         </ScrollView>
       </View>
     );
@@ -114,10 +158,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    height: 250,
+    height: 200,
   },
   buttonContainer: {
-    height:35,
+    height: 35,
     width: 35,
     overflow:'hidden',
     borderRadius:20,
@@ -172,6 +216,33 @@ const styles = StyleSheet.create({
     height: 50,
     borderColor:'#D3D3D3',
     marginHorizontal: 20,
+  },
+  listviewcontainer: {
+    flex: 1,
+    padding: 12,
+    flexDirection: 'column',
+    height: 200,
+  },
+  singleListContainer: {
+    flexDirection: 'row',
+    marginTop: 3,
+    marginBottom: 5,
+    height: 40,
+  },
+  audienceProfile: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginRight: 5,
+    marginRight: 15,
+  },
+  message: {
+
+  },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
   },
 });
 
