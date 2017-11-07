67c496dfba1f8ada0f5d64263f1e60d4224739f1
YingLiugh <ying.liu.18@dartmouth.edu>
2017-11-07 18:15:27 -0500

    allow text to be editable

--- miam/Components/Canvas.js
+++ miam/Components/Canvas.js
@@ -10,7 +10,6 @@ import {
   TextInput
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import Ionicon from "react-native-vector-icons/Ionicons";
 import StatusBarColor from "./StatusBarColor";
 import { ImagePicker } from "expo";
 import Heading from "./Heading";
@@ -25,11 +24,13 @@ class Canvas extends React.Component {
     this.state = {
       image: null,
       texts: [],
-      tags: []
+      tags: [],
+      text: "",
+      textInput: false
     };
     this.onCreatePost = this.onCreatePost.bind(this);
     this.getImageFromGiphy = this.getImageFromGiphy.bind(this);
-    this.createText = this.createText.bind(this);
+    this.createTextObj = this.createTextObj.bind(this);
   }
 
   componentDidMount() {
@@ -63,12 +64,15 @@ class Canvas extends React.Component {
     this.props.navigation.navigate("Search");
   }
 
-  createText(){
+  createTextObj() {
     this.setState(prevState => ({
-      texts: [...prevState.texts, <TxtCObj key={prevState.texts.length} text="Place Text Here!"/>]
+      text: "",
+      texts: [
+        ...prevState.texts,
+        <TxtCObj key={prevState.texts.length} text={this.state.text} />
+      ]
     }));
   }
-
   render() {
     return (
       <View style={styles.body}>
@@ -80,19 +84,66 @@ class Canvas extends React.Component {
               <Text style={{ fontSize: 25, color: "#cc66cc" }}>Canvas</Text>
               <Icon name="send" color="#ac3973" size={20} />
             </View>
-            {this.state.image && (
-              <View>
-                <Image
-                  source={{ uri: this.state.image }}
-                  style={styles.imagePreview}
-                />
-              </View>
-            )}
-            {
-              this.state.texts
-            }
+            <View style={{ height: "70%" }}>
+              {this.state.image && (
+                <View>
+                  <Image
+                    source={{ uri: this.state.image }}
+                    style={styles.imagePreview}
+                    resizeMode="contain"
+                  />
+                </View>
+              )}
+              {this.state.texts}
+            </View>
           </View>
           <View style={styles.tools}>
+            <View style={styles.textInput}>
+              <View style={styles.textIcon}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: "#cc66cc",
+                    textAlign: "center"
+                  }}
+                >
+                  T
+                </Text>
+              </View>
+              <TextInput
+                style={{
+                  width: "75%",
+                  borderColor: "gray",
+                  borderWidth: 1,
+                  height: "100%"
+                }}
+                onChangeText={text => this.setState({ text })}
+                value={this.state.text}
+              />
+              <TouchableHighlight
+                onPress={this.createTextObj}
+                underlayColor="white"
+                style={{
+                  width: "18%",
+                  justifyContent: "center",
+                  height: "100%"
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "#9999ff",
+                    height: "100%",
+                    justifyContent: "center"
+                  }}
+                >
+                  <Text style={{ textAlign: "center", color: "#ffffff" }}>
+                    add
+                  </Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+          </View>
+          <View style={styles.mainIcons}>
             <TouchableHighlight
               onPress={this.getImageFromRoll}
               underlayColor="white"
@@ -105,12 +156,6 @@ class Canvas extends React.Component {
               underlayColor="white"
             >
               <Icon name="gif" color="#ac3973" size={40} />
-            </TouchableHighlight>
-            <Icon name="brush" color="#ac3973" size={30} />
-            <TouchableHighlight
-              onPress={this.createText}
-              underlayColor="white">
-              <Ionicon name="ios-text" color="#ac3973" size={40} />
             </TouchableHighlight>
           </View>
         </View>
@@ -132,8 +177,8 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   canvas: {
-    width: "80%",
-    height: "80%",
+    width: "90%",
+    height: "65%",
     marginTop: "10%",
     borderWidth: 1,
     borderRadius: 3
@@ -144,16 +189,32 @@ const styles = StyleSheet.create({
   },
   tools: {
     flexDirection: "row",
-    marginBottom: "1%"
+    marginBottom: "1%",
+    height: "15%",
+    width: "90%",
+    borderWidth: 1,
+    borderRadius: 3
   },
   backgroundDeepPurple: {
     backgroundColor: "#695287"
   },
   imagePreview: {
-    width: 200,
+    width: 300,
     height: 200,
     alignSelf: "center",
     shadowOpacity: 0.3,
     shadowRadius: 3
+  },
+  textInput: {
+    flexDirection: "row",
+    height: "40%",
+    marginTop: "1%"
+  },
+  mainIcons: {
+    flexDirection: "row"
+  },
+  textIcon: {
+    width: "5%",
+    justifyContent: "center"
   }
 });
