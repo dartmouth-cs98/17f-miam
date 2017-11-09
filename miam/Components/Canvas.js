7ba7b19ecb86bc783dea24b8e67c40b8ed999056
EdwardEddyEd <Edward.J.Camp.18@dartmouth.edu>
2017-11-08 21:18:09 -0500

    Added Editing Functionality

--- miam/Components/Canvas.js
+++ miam/Components/Canvas.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Slider from "react-native-slider";
 
 import {
   StyleSheet,
@@ -10,6 +11,7 @@ import {
   TextInput
 } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import FIcon from "react-native-vector-icons/FontAwesome";
 import StatusBarColor from "./StatusBarColor";
 import { ImagePicker } from "expo";
 import Heading from "./Heading";
@@ -23,14 +25,24 @@ class Canvas extends React.Component {
     super(props);
     this.state = {
       image: null,
+      selected: null,
+      selectedType: null,
+      selectedText: "",
+      editMode: -1,
       texts: [],
       tags: [],
-      text: "",
-      textInput: false
+      createTextMode: false
     };
     this.onCreatePost = this.onCreatePost.bind(this);
     this.getImageFromGiphy = this.getImageFromGiphy.bind(this);
+    this.toggleCreateTextMode = this.toggleCreateTextMode.bind(this);
     this.createTextObj = this.createTextObj.bind(this);
+
+    // Functions for editing text
+    this.editTextSizeMode = this.editTextSizeMode.bind(this);
+    this.editTextRotationMode = this.editTextRotationMode.bind(this);
+    this.editTextColorMode = this.editTextColorMode.bind(this);
+    this.deleteText = this.deleteText.bind(this);
   }
 
   componentDidMount() {
@@ -64,27 +76,53 @@ class Canvas extends React.Component {
     this.props.navigation.navigate("Search");
   }
 
+  toggleCreateTextMode(){
+    if(this.state.selected)
+      this.setState({selected: null, selectedType: null, selectedText: "", editMode: -1});
+    this.setState(prevState => ({createTextMode: !prevState.createTextMode}));
+  }
+
   createTextObj() {
+    this.toggleCreateTextMode();
     this.setState(prevState => ({
-      text: "",
       texts: [
         ...prevState.texts,
-        <TxtCObj key={prevState.texts.length} text={this.state.text} />
+        <TxtCObj key={prevState.texts.length} text={this.state.text} canvas={this}/>
       ]
     }));
   }
+
+  editTextSizeMode(){
+    this.setState({ editMode: 2 });
+  }
+
+  editTextRotationMode(){
+    this.setState({ editMode: 3 });
+  }
+
+  editTextColorMode(){
+    this.setState({ editMode: 4 });
+  }
+
+  deleteText(){
+    this.state.selected.setState({text: ""});
+    this.setState({selected: null, selectedType: null, selectedText: "", editMode: -1});
+  }
+
   render() {
     return (
       <View style={styles.body}>
         <StatusBarColor />
         <Heading text="MiAM" />
+
+
         <View style={styles.canvasContainer}>
           <View style={styles.canvas}>
             <View style={styles.canvasHeading}>
               <Text style={{ fontSize: 25, color: "#cc66cc" }}>Canvas</Text>
               <Icon name="send" color="#ac3973" size={20} />
             </View>
-            <View style={{ height: "70%" }}>
+            <View style={{ height: "70%", overflow: 'hidden' }}>
               {this.state.image && (
                 <View>
                   <Image
@@ -97,25 +135,19 @@ class Canvas extends React.Component {
               {this.state.texts}
             </View>
           </View>
+
+
           <View style={styles.tools}>
+          {
+            this.state.createTextMode &&
             <View style={styles.textInput}>
-              <View style={styles.textIcon}>
-                <Text
-                  style={{
-                    fontSize: 20,
-                    color: "#cc66cc",
-                    textAlign: "center"
-                  }}
-                >
-                  T
-                </Text>
-              </View>
               <TextInput
                 style={{
                   width: "75%",
                   borderColor: "gray",
                   borderWidth: 1,
-                  height: "100%"
+                  height: "100%",
+                  left: "20%"
                 }}
                 onChangeText={text => this.setState({ text })}
                 value={this.state.text}
@@ -142,7 +174,102 @@ class Canvas extends React.Component {
                 </View>
               </TouchableHighlight>
             </View>
+          }
+          {
+            this.state.selected && 
+            <View style={styles.editorButtonBox}>
+              <View style={{height: '100%', flex: 1, flexDirection: 'column'}}>
+                <View style={styles.editorButtonRow}>
+                  <TouchableHighlight onPress={this.editTextSizeMode} underlayColor="white">
+                    <FIcon name="text-height" color="#ac3973" size={35} />
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={this.editTextRotationMode} underlayColor="white">
+                    <Icon name="autorenew" color="#ac3973" size={40} />
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={this.editTextColorMode} underlayColor="white">
+                    <Icon name="color-lens" color="#ac3973" size={40} />
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={this.deleteText} underlayColor="white">
+                    <Icon name="delete" color="white" size={40} style={{backgroundColor: "red", borderRadius: 50}}/>
+                  </TouchableHighlight>
+                </View>
+                <View style={styles.editorButtonRow}>
+                  <TouchableHighlight onPress={() => {}} underlayColor="white">
+                    <Icon name="not-interested" color="#ac3973" size={35} />
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={() => {}} underlayColor="white">
+                    <Icon name="not-interested" color="#ac3973" size={40} />
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={() => {}} underlayColor="white">
+                    <Icon name="not-interested" color="#ac3973" size={40} />
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={() => {}} underlayColor="white">
+                    <Icon name="not-interested" color="#ac3973" size={40} />
+                  </TouchableHighlight>
+                </View>
+              </View>
+              <View style={{height: '100%', flex: 1}}>
+              {
+                this.state.editMode == 2 &&
+                <View style={styles.editorUIBox}>
+                  <Text style={{flex: 1}}>TEXT SIZE:</Text>
+                  <Slider
+                    flex={2}
+                    value={this.state.selected.state.fontSize}
+                    maximumValue={50}
+                    minimumValue={10}
+                    step={1}
+                    onValueChange={(value) => this.state.selected.setState({fontSize: value})} />
+                </View>
+              }
+              {
+                this.state.editMode == 3 &&
+                <View style={styles.editorUIBox}>
+                  <Text style={{flex: 1}}>ROTATE:</Text>
+                  <Slider
+                    flex={2}
+                    value={this.state.selected.state.rotation}
+                    maximumValue={360}
+                    minimumValue={0}
+                    step={15}
+                    onValueChange={(value) => this.state.selected.setState({rotation: value})} />
+                </View>
+              }
+              {
+                this.state.editMode == 4 &&
+                <View style={styles.editorUIBox}>
+                  <Slider
+                    flex={1}
+                    value={this.state.selected.state.red}
+                    maximumValue={255}
+                    minimumValue={0}
+                    step={1}
+                    thumbTintColor="#FF0000"
+                    onValueChange={(value) => this.state.selected.setState({red: value})} />
+                    <Slider
+                    flex={1}
+                    value={this.state.selected.state.green}
+                    maximumValue={255}
+                    minimumValue={0}
+                    step={1}
+                    thumbTintColor="#00AA00"
+                    onValueChange={(value) => this.state.selected.setState({green: value})} />
+                    <Slider
+                    flex={1}
+                    value={this.state.selected.state.blue}
+                    maximumValue={255}
+                    minimumValue={0}
+                    step={1}
+                    thumbTintColor="#0000FF"
+                    onValueChange={(value) => this.state.selected.setState({blue: value})} />
+                </View>
+              }
+              </View>
+            </View>
+          }
           </View>
+
+
           <View style={styles.mainIcons}>
             <TouchableHighlight
               onPress={this.getImageFromRoll}
@@ -156,6 +283,13 @@ class Canvas extends React.Component {
               underlayColor="white"
             >
               <Icon name="gif" color="#ac3973" size={40} />
+            </TouchableHighlight>
+
+            <TouchableHighlight
+              onPress={this.toggleCreateTextMode}
+              underlayColor="white"
+            >
+              <Icon name="text-fields" color="#ac3973" size={40} />
             </TouchableHighlight>
           </View>
         </View>
@@ -208,7 +342,30 @@ const styles = StyleSheet.create({
   textInput: {
     flexDirection: "row",
     height: "40%",
-    marginTop: "1%"
+    marginTop: "1%",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  editorButtonRow: {
+    width: '100%', 
+    flex: 1, 
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  editorButtonBox: {
+    flexDirection: 'row', 
+    width: '100%', 
+    height: '100%'
+  },
+  editorUIBox:{
+    width: '90%', 
+    flexDirection: 'column', 
+    flex: 1, 
+    alignSelf: 'center', 
+    alignItems: 'stretch', 
+    justifyContent: 'center'
   },
   mainIcons: {
     flexDirection: "row"
--- miam/Components/CanvasObjects/TextCanvasObj.js
+++ miam/Components/CanvasObjects/TextCanvasObj.js
@@ -13,8 +13,17 @@ class TextCanvasObj extends React.Component{
 	constructor(props){
 	    super(props);
 	    this.state = {
+	    	type: "text",
+	    	red: 0,
+	    	green: 0,
+	    	blue: 0,
+	    	rotation: 0,
+	    	fontSize: 20,
+	    	canvas: props.canvas || null,
 	    	text: props.text || ""
 	    };
+
+	    this.id = props.id;
 	}
 
 	componentWillMount(){
@@ -23,8 +32,13 @@ class TextCanvasObj extends React.Component{
 
 	    this._animatedValue.addListener((value) => this._value = value);
 	    this._panResponder = PanResponder.create({
+	    	onStartShouldSetPanResponder: (evt, gestureState) => true,
+      		onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
 	        onMoveShouldSetResponderCapture: () => true, //Tell iOS that we are allowing the movement
 	        onMoveShouldSetPanResponderCapture: () => true, // Same here, tell iOS that we allow dragging
+	        onPanResponderStart: (e, gestureState) => {
+			  this.state.canvas.setState({ selected: this, selectedType: this.state.type, selectedText: this.state.text, createTextMode: false});
+	        },
 	        onPanResponderGrant: (e, gestureState) => {
 	          this._animatedValue.setOffset({x: this._value.x, y: this._value.y});
 	          this._animatedValue.setValue({x: 0, y: 0});
@@ -39,21 +53,34 @@ class TextCanvasObj extends React.Component{
 	}
 
     render(){
+
         return (
-            <View style={styles.container}>
 	        <Animated.View 
 	          style={
 	              {
 	                transform: [
 	                  {translateX: this._animatedValue.x},
 	                  {translateY: this._animatedValue.y},
+	                  {rotate: this.state.rotation + 'deg'}
 	                ],
-	                backgroundColor: '#00000000'
+	                backgroundColor: '#00000000',
+	                position: 'absolute',
+	                alignSelf: 'center',
+	                bottom: '50%'
 	              }} 
 	            {...this._panResponder.panHandlers}>
-	            <Text style={styles.textStyle}>{this.state.text}</Text>
+	            <Text style={
+	            	{
+	            		color: 'rgb(' + this.state.red + ',' + this.state.green + ',' + this.state.blue + ')',
+	            		fontSize: this.state.fontSize,
+	            		fontWeight: 'bold',
+	            		textAlign: "center",
+	            		borderBottomWidth: 1
+	            	}
+	            }>
+	            	{this.state.text}
+	            </Text>
 	        </Animated.View>
-	      	</View>
         );
     }
 }
@@ -63,13 +90,7 @@ const styles = StyleSheet.create({
     	flex: 1,
     	alignItems: 'center',
     	justifyContent: 'center'
-  	},
-	textStyle: {
-	    color: "#000000",
-	    fontSize: 20,
-	    fontWeight: 'bold',
-	    textAlign: "center"
-	},
+  	}
 });
 
 export default TextCanvasObj;
